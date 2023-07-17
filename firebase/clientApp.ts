af2d532632c4a29666f2eddca810b4df960d8374
ZineddineBk09 <eino1209@gmail.com>
2023-07-17 11:52:10 +0100

    add firebase

---
+++ firebase/clientApp.ts
@@ -0,0 +1,23 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase this way to avoid multiple instances of firebase app being created
+export const firebaseApp = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApp();
+export const firestore = getFirestore(firebaseApp);
+export const storage = getStorage(firebaseApp);
+
+export default { firebaseApp, firestore, storage };
