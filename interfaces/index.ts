5821939288cdef96c60b590a3082f97b6b3adfd5
ZineddineBk09 <eino1209@gmail.com>
2023-07-18 19:21:17 +0100

    fix

---
+++ interfaces/index.ts
@@ -0,0 +1,18 @@
+export interface BlogPost {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  date: string;
+}
+
+export interface Article {
+  id: string;
+  banner: string;
+  body?: string;
+  brief: string;
+  category: string;
+  postLength: string;
+  published: Date;
+  title: string;
+}
