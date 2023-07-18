86802747d35874afd40535c86fc4dccb598c83cf
ZineddineBk09 <eino1209@gmail.com>
2023-07-18 19:21:02 +0100

    fix

---
+++ components/posts/PostCard.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Article } from "@/interfaces";
+
+const styles = {
+  wrapper: `max-w-[46rem] h-[10rem] flex items-center gap-[1rem] cursor-pointer`,
+  postDetails: `flex-[2.5] flex flex-col`,
+  authorContainer: `flex gap-[.4rem]`,
+  authorName: `font-semibold`,
+  authorImageContainer: `grid place-items-center rounded-full overflow-hidden h-[1.4rem] w-[1.4rem]`,
+  authorImage: `object-cover`,
+  title: `font-bold text-2xl`,
+  briefing: `text-[#787878]`,
+  detailsContainer: `flex items-center justify-between text-[#787878]`,
+  articleDetails: `my-2 text-[.8rem]`,
+  bookmarkContainer: `cursor-pointer`,
+  category: `bg-[#F2F3F2] p-1 rounded-full`,
+  thumbnailContainer: `flex-1`,
+};
+
+const PostCard = ({ post }: { post: Article }) => {
+  console.log("Post: ", post);
+  return (
+    <Link href={`/blogs/${post.id}`}>
+      <div className={styles.wrapper}>
+        <div className={styles.postDetails}>
+          <div className={styles.authorContainer}>
+            <div className={styles.authorImageContainer}>
+              <Image
+                src={`/images/logo.png`}
+                alt="Venlo Seeds"
+                className={styles.authorImage}
+                height={40}
+                width={40}
+              />
+            </div>
+            <div className={styles.authorName}>Venlo Seeds</div>
+          </div>
+          <h1 className={styles.title}>{post.title}</h1>
+          <div className={styles.briefing}>{post.brief}</div>
+          <div className={styles.detailsContainer}>
+            <span className={styles.articleDetails}>
+              {new Date(post.published).toLocaleString("en-US", {
+                day: "numeric",
+                month: "short",
+              })}
+              • {post.postLength} •{" "}
+              <span className={styles.category}>{post.category}</span>
+            </span>
+          </div>
+        </div>
+        <div className={styles.thumbnailContainer}>
+          <Image
+            src={post?.banner || ""}
+            alt="thumbnail"
+            height={100}
+            width={100}
+            className="rounded-md"
+          />
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default PostCard;
---
+++ components/register/RegisterForm.tsx
@@ -0,0 +1,106 @@
+import Link from "next/link";
+import React from "react";
+
+const RegisterForm = () => {
+  return (
+    <section className="bg-gradient-to-b from-gray-100 to-white">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+          {/* Page header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h1 className="h1">
+              Welcome.
+            </h1>
+          </div>
+
+          {/* Form */}
+          <div className="max-w-sm mx-auto">
+            <form>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="name"
+                  >
+                    Name <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    className="form-input w-full text-gray-800"
+                    placeholder="Enter your name"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="email"
+                  >
+                    Email <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    className="form-input w-full text-gray-800"
+                    placeholder="Enter your email address"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mb-4">
+                <div className="w-full px-3">
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="password"
+                  >
+                    Password <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="password"
+                    type="password"
+                    className="form-input w-full text-gray-800"
+                    placeholder="Enter your password"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="flex flex-wrap -mx-3 mt-6">
+                <div className="w-full px-3">
+                  <button className="btn text-white bg-green-600 hover:bg-green-700 w-full">
+                    Sign up
+                  </button>
+                </div>
+              </div>
+              <div className="text-sm text-gray-500 text-center mt-3">
+                By creating an account, you agree to the{" "}
+                <a className="underline" href="#0">
+                  terms & conditions
+                </a>
+                , and our{" "}
+                <a className="underline" href="#0">
+                  privacy policy
+                </a>
+                .
+              </div>
+            </form>
+
+            <div className="text-gray-600 text-center mt-6">
+              Already using Venlo Seeds?{" "}
+              <Link
+                href="/signin"
+                className="text-green-600 hover:underline transition duration-150 ease-in-out"
+              >
+                Sign in
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RegisterForm;
