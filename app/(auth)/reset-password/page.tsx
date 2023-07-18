6633a54896311565e7f43e36c45dcfb38b98ff28
ZineddineBk09 <eino1209@gmail.com>
2023-07-18 19:21:31 +0100

    fix

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,18 +1,21 @@
 export const metadata = {
-  title: 'Reset Password - Simple',
-  description: 'Page description',
-}
+  title: "Reset Password | Venlo Seeds",
+  description:
+    "Forgot your password? Reset it securely and regain access to your Venlo Seeds account. Follow the simple steps to reset your password and continue enjoying all the benefits of our platform.",
+};
 
 export default function ResetPassword() {
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
           {/* Page header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h1 className="h1 mb-4">Let's get you back up on your feet</h1>
-            <p className="text-xl text-gray-600">Enter the email address you used when you signed up for your account, and we'll email you a link to reset your password.</p>
+            <p className="text-xl text-gray-600">
+              Enter the email address you used when you signed up for your
+              account, and we'll email you a link to reset your password.
+            </p>
           </div>
 
           {/* Form */}
@@ -20,20 +23,32 @@ export default function ResetPassword() {
             <form>
               <div className="flex flex-wrap -mx-3 mb-4">
                 <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email <span className="text-red-600">*</span></label>
-                  <input id="email" type="email" className="form-input w-full text-gray-800" placeholder="Enter your email address" required />
+                  <label
+                    className="block text-gray-800 text-sm font-medium mb-1"
+                    htmlFor="email"
+                  >
+                    Email <span className="text-red-600">*</span>
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    className="form-input w-full text-gray-800"
+                    placeholder="Enter your email address"
+                    required
+                  />
                 </div>
               </div>
               <div className="flex flex-wrap -mx-3 mt-6">
                 <div className="w-full px-3">
-                  <button className="btn text-white bg-green-600 hover:bg-green-700 w-full">Send reset link</button>
+                  <button className="btn text-white bg-green-600 hover:bg-green-700 w-full">
+                    Send reset link
+                  </button>
                 </div>
               </div>
             </form>
           </div>
-
         </div>
       </div>
     </section>
-  )
+  );
 }
