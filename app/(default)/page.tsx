ee1dcbdb3e2209c56cf25f5492ed84fd24636ed3
ZineddineBk09 <eino1209@gmail.com>
2023-07-18 19:20:15 +0100

    fix

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,5 +1,5 @@
 export const metadata = {
-  title: 'Home - Simple',
+  title: 'Home | Venlo Seeds',
   description: 'Page description',
 }
 
---
+++ app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import BlogPosts from "@/components/posts/Posts";
+
+export const metadata = {
+  title: "Blogs | Venlo Seeds",
+  description:
+    "Discover a wide range of informative and engaging blogs about plants, gardening, and seeds on Venlo Seeds. Expand your knowledge and enhance your gardening skills with our insightful articles.",
+};
+
+const BlogsPage = () => {
+  return <BlogPosts />;
+};
+
+export default BlogsPage;
