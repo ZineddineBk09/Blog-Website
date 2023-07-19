5a5161683d96982587e156df33d1d4188f47e146
ZineddineBk09 <eino1209@gmail.com>
2023-07-19 11:54:32 +0100

    fix

--- components/modals/UpdateModal.tsx
+++ components/modals/UpdateModal.tsx
@@ -15,6 +15,7 @@ import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/navigation";
 import TextEditor from "../blogs/Editor";
 import { Article } from "@/interfaces";
+import LoadingSpinner from "../LoadingSpinner";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
@@ -27,16 +28,17 @@ const Transition = React.forwardRef(function Transition(
 
 type Props = {
   post: Article;
+  refresh: () => void;
 };
 
-export default function UpdateModal({ post }: Props) {
+export default function UpdateModal({ post, refresh }: Props) {
   const [loading, setLoading] = React.useState(false);
   const [open, setOpen] = React.useState(false);
   const navigate = useRouter();
   // handle banner and body separately
   const [body, setBody] = React.useState("");
   const [banner, setBanner] = React.useState<File>();
-  const [bannerUrl, setBannerUrl] = React.useState<string>(post.banner);
+  const [bannerUrl, setBannerUrl] = React.useState<string>("");
 
   const handleUploadImage = async (id: string) => {
     if (!banner) return;
@@ -66,21 +68,29 @@ export default function UpdateModal({ post }: Props) {
     }),
     onSubmit: async (values) => {
       setLoading(true);
+      // check if banner is updated
+      if (post.banner !== bannerUrl) {
+        console.log("Banner is updated");
+        await handleUploadImage(post.id.toString());
+      }
+
       // update the product doc on firestore
-      await addDoc(collection(firestore, "blogs"), {
-        ...values,
-        body,
-        published: new Date(),
+      await updateDoc(doc(firestore, "blogs", post.id.toString()), {
+        title: values.title,
+        brief: values.brief,
+        category: values.category,
+        postLength: values.postLength,
+        body: body,
       })
-        .then((docRef) => {
-          console.log("Document written with ID: ", docRef.id);
-          handleUploadImage(docRef.id);
+        .then(async () => {
+          // close the modal
+          await refresh();
           setLoading(false);
-          navigate.push("/blogs");
+          handleClose();
         })
         .catch((error) => {
           setLoading(false);
-          console.log("Error adding blog: ", error);
+          console.log("Error updating blog: ", error);
         });
     },
   });
@@ -130,172 +140,176 @@ export default function UpdateModal({ post }: Props) {
       >
         <DialogContent>
           <div className="mx-auto">
-            <form
-              onSubmit={formik.handleSubmit}
-              className="grid grid-cols-1 gap-2 md:grid-cols-2"
-            >
-              {/* Banner */}
-              <div className="flex flex-wrap -mx-3 mb-4 col-span-2 relative">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1">
-                    Banner <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    id="banner"
-                    name="banner"
-                    type="file"
-                    className="form-input w-full text-gray-800 mb-3"
-                    placeholder="Upload a banner image"
-                    onChange={handleChangeImage}
-                  />
-                  {bannerUrl && (
-                    <Image
-                      src={bannerUrl}
-                      width={3008}
-                      height={1579}
-                      alt="banner"
-                      className="form-input object-cover w-full h-96 rounded-lg"
+            {loading ? (
+              <LoadingSpinner />
+            ) : (
+              <form
+                onSubmit={formik.handleSubmit}
+                className="grid grid-cols-1 gap-2 md:grid-cols-2"
+              >
+                {/* Banner */}
+                <div className="flex flex-wrap -mx-3 mb-4 col-span-2 relative">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1">
+                      Banner <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      id="banner"
+                      name="banner"
+                      type="file"
+                      className="form-input w-full text-gray-800 mb-3"
+                      placeholder="Upload a banner image"
+                      onChange={handleChangeImage}
                     />
-                  )}
+                    {bannerUrl && (
+                      <Image
+                        src={bannerUrl}
+                        width={3008}
+                        height={1579}
+                        alt="banner"
+                        className="form-input object-cover w-full h-96 rounded-lg"
+                      />
+                    )}
+                  </div>
                 </div>
-              </div>
 
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1">
-                    Title <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    id="title"
-                    name="title"
-                    type="text"
-                    placeholder="Blog Title"
-                    value={formik.values.title}
-                    className={
-                      "form-input w-full text-gray-800" +
-                      " " +
-                      (formik.touched.title && formik.errors.title
-                        ? "border border-red-500"
-                        : "")
-                    }
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  <p className="text-red-500 text-xs">
-                    {formik.touched.title && formik.errors.title
-                      ? formik.errors.title
-                      : ""}
-                  </p>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1">
+                      Title <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      id="title"
+                      name="title"
+                      type="text"
+                      placeholder="Blog Title"
+                      value={formik.values.title}
+                      className={
+                        "form-input w-full text-gray-800" +
+                        " " +
+                        (formik.touched.title && formik.errors.title
+                          ? "border border-red-500"
+                          : "")
+                      }
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <p className="text-red-500 text-xs">
+                      {formik.touched.title && formik.errors.title
+                        ? formik.errors.title
+                        : ""}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1">
-                    Brief <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    id="brief"
-                    name="brief"
-                    type="text"
-                    placeholder="Enter a brief description of your blog"
-                    value={formik.values.brief}
-                    className={
-                      "form-input w-full text-gray-800" +
-                      " " +
-                      (formik.touched.brief && formik.errors.brief
-                        ? "border border-red-500"
-                        : "")
-                    }
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  <p className="text-red-500 text-xs">
-                    {formik.touched.brief && formik.errors.brief
-                      ? formik.errors.brief
-                      : ""}
-                  </p>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1">
+                      Brief <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      id="brief"
+                      name="brief"
+                      type="text"
+                      placeholder="Enter a brief description of your blog"
+                      value={formik.values.brief}
+                      className={
+                        "form-input w-full text-gray-800" +
+                        " " +
+                        (formik.touched.brief && formik.errors.brief
+                          ? "border border-red-500"
+                          : "")
+                      }
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <p className="text-red-500 text-xs">
+                      {formik.touched.brief && formik.errors.brief
+                        ? formik.errors.brief
+                        : ""}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1">
-                    Category <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    id="category"
-                    name="category"
-                    type="text"
-                    placeholder="Farming, Growing, etc."
-                    value={formik.values.category}
-                    className={
-                      "form-input w-full text-gray-800" +
-                      " " +
-                      (formik.touched.category && formik.errors.category
-                        ? "border border-red-500"
-                        : "")
-                    }
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  <p className="text-red-500 text-xs">
-                    {formik.touched.category && formik.errors.category
-                      ? formik.errors.category
-                      : ""}
-                  </p>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1">
+                      Category <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      id="category"
+                      name="category"
+                      type="text"
+                      placeholder="Farming, Growing, etc."
+                      value={formik.values.category}
+                      className={
+                        "form-input w-full text-gray-800" +
+                        " " +
+                        (formik.touched.category && formik.errors.category
+                          ? "border border-red-500"
+                          : "")
+                      }
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <p className="text-red-500 text-xs">
+                      {formik.touched.category && formik.errors.category
+                        ? formik.errors.category
+                        : ""}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1">
-                    Post length <span className="text-red-600">*</span>
-                  </label>
-                  <input
-                    id="postLength"
-                    name="postLength"
-                    type="text"
-                    placeholder="15 min read, 5 min read, etc."
-                    value={formik.values.postLength}
-                    className={
-                      "form-input w-full text-gray-800" +
-                      " " +
-                      (formik.touched.postLength && formik.errors.postLength
-                        ? "border border-red-500"
-                        : "")
-                    }
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  <p className="text-red-500 text-xs">
-                    {formik.touched.postLength && formik.errors.postLength
-                      ? formik.errors.postLength
-                      : ""}
-                  </p>
+                <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label className="block text-gray-800 text-sm font-medium mb-1">
+                      Post length <span className="text-red-600">*</span>
+                    </label>
+                    <input
+                      id="postLength"
+                      name="postLength"
+                      type="text"
+                      placeholder="15 min read, 5 min read, etc."
+                      value={formik.values.postLength}
+                      className={
+                        "form-input w-full text-gray-800" +
+                        " " +
+                        (formik.touched.postLength && formik.errors.postLength
+                          ? "border border-red-500"
+                          : "")
+                      }
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    <p className="text-red-500 text-xs">
+                      {formik.touched.postLength && formik.errors.postLength
+                        ? formik.errors.postLength
+                        : ""}
+                    </p>
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mb-4 col-span-2">
-                <div className="w-full px-3">
-                  <label
-                    className="block text-gray-800 text-sm font-medium mb-1"
-                    htmlFor="email"
-                  >
-                    Body <span className="text-red-600">*</span>
-                  </label>
-                  <TextEditor setBody={setBody} body={body} />
+                <div className="flex flex-wrap -mx-3 mb-4 col-span-2">
+                  <div className="w-full px-3">
+                    <label
+                      className="block text-gray-800 text-sm font-medium mb-1"
+                      htmlFor="email"
+                    >
+                      Body <span className="text-red-600">*</span>
+                    </label>
+                    <TextEditor setBody={setBody} body={body} />
+                  </div>
                 </div>
-              </div>
-              <div className="flex flex-wrap -mx-3 mt-6 col-span-2">
-                <div className="w-full px-3">
-                  <button
-                    type="submit"
-                    className={
-                      "btn text-white bg-green-600 hover:bg-green-700 w-full"
-                    }
-                  >
-                    {loading ? "Publishing..." : "Publish"}
-                  </button>
+                <div className="flex flex-wrap -mx-3 mt-6 col-span-2">
+                  <div className="w-full px-3">
+                    <button
+                      type="submit"
+                      className={
+                        "btn text-white bg-green-600 hover:bg-green-700 w-full"
+                      }
+                    >
+                      {loading ? "Publishing..." : "Publish"}
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
+              </form>
+            )}
           </div>
         </DialogContent>
       </Dialog>
